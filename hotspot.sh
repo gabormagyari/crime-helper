2dd8a6e0f0b489c736da4b57905970dc71aea55b
Gabor Magyari <gabor.magyari@logmein.com>
2018-11-14 15:46:47 +0100

    added cloc

--- hotspot.sh
+++ hotspot.sh
@@ -9,5 +9,6 @@ mkdir output
 
 cd $repository
 git log --pretty=format:'[%h] %aN %ad %s' --date=short --numstat --after=$afterDate > $scriptPath/working/git-evo.log
+cloc ./ --unix --by-file --csv --quiet > $scriptPath/working/git-loc.csv
 cd $scriptPath
-java -jar libs/code-maat-1.1-SNAPSHOT-standalone.jar -l $scriptPath/working/git-evo.log -c git -a revisions > $scriptPath/working/git-rev.log
+java -jar libs/code-maat-1.1-SNAPSHOT-standalone.jar -l $scriptPath/working/git-evo.log -c git -a revisions > $scriptPath/working/git-rev.csv
