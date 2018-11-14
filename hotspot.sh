6334c3be129551c54f7f69dc2062339064bfe169
Gabor Magyari <gabor.magyari@logmein.com>
2018-11-14 16:00:25 +0100

    added merge script

--- hotspot.sh
+++ hotspot.sh
@@ -10,5 +10,7 @@ mkdir output
 cd $repository
 git log --pretty=format:'[%h] %aN %ad %s' --date=short --numstat --after=$afterDate > $scriptPath/working/git-evo.log
 cloc ./ --unix --by-file --csv --quiet > $scriptPath/working/git-loc.csv
+
 cd $scriptPath
-java -jar libs/code-maat-1.1-SNAPSHOT-standalone.jar -l $scriptPath/working/git-evo.log -c git -a revisions > $scriptPath/working/git-rev.csv
+java -jar libs/code-maat-1.1-SNAPSHOT-standalone.jar -l ./working/git-evo.log -c git -a revisions > ./working/git-rev.csv
+python libs/merge_comp_freqs.py ./working/git-rev.csv ./working/git-loc.csv > ./output/hotspot.csv
